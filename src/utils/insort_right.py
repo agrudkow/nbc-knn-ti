de6eafd4cc17e796ce4cf149c4c9e75d138b6bb0
Artur Grudkowski <agrudkow@gmail.com>
2021-08-13 09:53:34 +0200

    feat(test): add performance tests and associated results

--- src/utils/insort_right.py
+++ src/utils/insort_right.py
@@ -7,3 +7,5 @@ T = TypeVar('T')
 def insort_right(data: List[T], item: T, key: Callable[[str], int]):
   keys = [key(r) for r in data]
   data.insert(bisect_right(keys, key(item)), item)
+  # data.append(item)
+  # data.sort(key=key)
